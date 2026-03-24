6515f521a90cddbc063aa124b61aed27c33d1218
DESKTOP-AKE1CR0\22891 <2289170272@qq.com>
2026-03-24 15:40:02 +0800

    更新Addressable UI文件夹

--- Assets/_CALSS_S1/Scripts/UIPages/LogoView.cs
+++ Assets/_CALSS_S1/Scripts/UIPages/LogoView.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using DG.Tweening;
+using System.Collections.Generic;
 using UnityEngine;
 
 public partial class LogoPage
 {
+	private float showSecond = 10f;
 	public LogoPage() : base(UIType.Normal, UIMode.DoNothing, UICollider.None)
 	{
 		Debug.LogWarning("TODO: 请修改LogoPage页面类型等参数，或注释此行");
@@ -11,6 +13,7 @@ public partial class LogoPage
 	public void OnStart()
 	{
 		//KBEngine.Event.registerOut("MyEventName", this, "MyEventHandler");
+		slider.DOValue(1, showSecond);
 	}
 
 	//public void MyEventHandler()
